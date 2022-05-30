bb1111429afaa9d3c759fc5e2e1c835eb60d3c52
JLague <justin.lague.1@ulaval.ca>
2022-05-30 09:48:12 -0400

    add script to distort entire dataset

---
+++ data/apply_fisheye.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import pathlib
+import cv2
+from ..utils import distort_image
+
+IN_PATH = "./tiny-imagenet-200"
+OUT_PATH = "./tiny-imagenet-200-fisheye"
+
+F = 50
+D = [0.5, 0.5, 0.5, 0.5]
+
+
+def main():
+    convert_dataset(IN_PATH, OUT_PATH)
+
+def convert_dataset(in_dir, out_dir):
+    in_dir = pathlib.Path(in_dir)
+    out_dir = pathlib.Path(out_dir)
+    
+    out_dir.mkdir(exist_ok=True)
+
+    for fpath in in_dir.rglob('*'):
+        if fpath.suffix == '.JPEG':
+            # Compute and create paths
+            relpath = fpath.relative_to(in_dir)
+            outpath = out_dir.joinpath(relpath)
+            outpath.parent.mkdir(parents=True, exist_ok=True)
+
+            # print('Creating', outpath)
+
+            # Distort image
+            img = cv2.imread(str(fpath))
+            distorted = distort_image(img, F, D)
+            cv2.imwrite(str(outpath), distorted)
+
+
+if __name__=='__main__':
+    main()
