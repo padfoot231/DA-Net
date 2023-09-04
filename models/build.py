4c9f09c6103b75d231e664e683d217cba15efbf8
aathwale5834 <akshaya.15je001564@am.ism.ac.in>
2023-09-04 17:39:44 -0400

    deleted useless model files

--- models/build.py
+++ models/build.py
@@ -23,10 +23,6 @@ import numpy as np
 from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNorm
 from torch.nn.modules.utils import _pair
 from scipy import ndimage
-from .swin_transformer import SwinTransformer
-from .swin_transformer_v2 import SwinTransformerV2
-from .swin_mlp import SwinMLP
-from .swin_transformer_az import SwinTransformerAz
 from .swin_transformer_angular import SwinTransformerAng
 
 
@@ -55,77 +51,6 @@ class SwinUnet(nn.Module):
                                     distortion_model = config.MODEL.DISTORTION, 
                                     n_radius = config.MODEL.NRADIUS, 
                                     n_azimuth = config.MODEL.NAZIMUTH) 
-
-        elif model_type == 'swin':
-            self.swin_unet = SwinTransformer(img_size=config.DATA.IMG_SIZE,
-                                    radius_cuts=config.MODEL.SWIN.RADIUS_CUTS, 
-                                    azimuth_cuts=config.MODEL.SWIN.AZIMUTH_CUTS,
-                                    in_chans=config.MODEL.SWIN.IN_CHANS,
-                                    num_classes=config.MODEL.NUM_CLASSES,
-                                    embed_dim=config.MODEL.SWIN.EMBED_DIM,
-                                    depths=config.MODEL.SWIN.DEPTHS,
-                                    num_heads=config.MODEL.SWIN.NUM_HEADS,
-                                    window_size=config.MODEL.SWIN.WINDOW_SIZE,
-                                    mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
-                                    qkv_bias=config.MODEL.SWIN.QKV_BIAS,
-                                    qk_scale=config.MODEL.SWIN.QK_SCALE,
-                                    drop_rate=config.MODEL.DROP_RATE,
-                                    drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                                    ape=config.MODEL.SWIN.APE,
-                                    patch_norm=config.MODEL.SWIN.PATCH_NORM,
-                                    use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                                    distortion_model = config.MODEL.DISTORTION)
-        elif model_type == 'swin_az':
-            self.swin_unet = SwinTransformerAz(img_size=config.DATA.IMG_SIZE,
-                                    radius_cuts=config.MODEL.SWIN.RADIUS_CUTS, 
-                                    azimuth_cuts=config.MODEL.SWIN.AZIMUTH_CUTS,
-                                    in_chans=config.MODEL.SWIN.IN_CHANS,
-                                    num_classes=config.MODEL.NUM_CLASSES,
-                                    embed_dim=config.MODEL.SWIN.EMBED_DIM,
-                                    depths=config.MODEL.SWIN.DEPTHS,
-                                    num_heads=config.MODEL.SWIN.NUM_HEADS,
-                                    window_size=config.MODEL.SWIN.WINDOW_SIZE,
-                                    mlp_ratio=config.MODEL.SWIN.MLP_RATIO,
-                                    qkv_bias=config.MODEL.SWIN.QKV_BIAS,
-                                    qk_scale=config.MODEL.SWIN.QK_SCALE,
-                                    drop_rate=config.MODEL.DROP_RATE,
-                                    drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                                    ape=config.MODEL.SWIN.APE,
-                                    patch_norm=config.MODEL.SWIN.PATCH_NORM,
-                                    use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                                    distortion_model = config.MODEL.DISTORTION)
-        elif model_type == 'swinv2':
-            self.swin_unet = SwinTransformerV2(img_size=config.DATA.IMG_SIZE,
-                                    patch_size=config.MODEL.SWINV2.PATCH_SIZE,
-                                    in_chans=config.MODEL.SWINV2.IN_CHANS,
-                                    num_classes=config.MODEL.NUM_CLASSES,
-                                    embed_dim=config.MODEL.SWINV2.EMBED_DIM,
-                                    depths=config.MODEL.SWINV2.DEPTHS,
-                                    num_heads=config.MODEL.SWINV2.NUM_HEADS,
-                                    window_size=config.MODEL.SWINV2.WINDOW_SIZE,
-                                    mlp_ratio=config.MODEL.SWINV2.MLP_RATIO,
-                                    qkv_bias=config.MODEL.SWINV2.QKV_BIAS,
-                                    drop_rate=config.MODEL.DROP_RATE,
-                                    drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                                    ape=config.MODEL.SWINV2.APE,
-                                    patch_norm=config.MODEL.SWINV2.PATCH_NORM,
-                                    use_checkpoint=config.TRAIN.USE_CHECKPOINT,
-                                    pretrained_window_sizes=config.MODEL.SWINV2.PRETRAINED_WINDOW_SIZES)
-        elif model_type == 'swin_mlp':
-            self.swin_unet = SwinMLP(img_size=config.DATA.IMG_SIZE,
-                            patch_size=config.MODEL.SWIN_MLP.PATCH_SIZE,
-                            in_chans=config.MODEL.SWIN_MLP.IN_CHANS,
-                            num_classes=config.MODEL.NUM_CLASSES,
-                            embed_dim=config.MODEL.SWIN_MLP.EMBED_DIM,
-                            depths=config.MODEL.SWIN_MLP.DEPTHS,
-                            num_heads=config.MODEL.SWIN_MLP.NUM_HEADS,
-                            window_size=config.MODEL.SWIN_MLP.WINDOW_SIZE,
-                            mlp_ratio=config.MODEL.SWIN_MLP.MLP_RATIO,
-                            drop_rate=config.MODEL.DROP_RATE,
-                            drop_path_rate=config.MODEL.DROP_PATH_RATE,
-                            ape=config.MODEL.SWIN_MLP.APE,
-                            patch_norm=config.MODEL.SWIN_MLP.PATCH_NORM,
-                            use_checkpoint=config.TRAIN.USE_CHECKPOINT)
         else:
             raise NotImplementedError(f"Unkown model: {model_type}")
 
