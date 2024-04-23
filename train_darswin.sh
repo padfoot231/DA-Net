e697f3faf7626dedf1bee955764e897f74b4a584
padfoot231 <akshaya.athwale.1@ulaval.ca>
2024-04-22 22:44:11 -0400

    darswin

---
+++ train_darswin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+export WANDB_MODE="disabled"
+# export NCCL_BLOCKING_WAIT=1 
+# export NCCL_DEBUG=INFO
+# export PYTHONFAULTHANDLER=1
+
+
+python -m torch.distributed.launch \
+--nproc_per_node 4 \
+--master_port 12345  main.py \
+--cfg configs/swin/CVRG_25_4.yaml \
+--output /home/prongs/scratch/Radial-unet \
+--data-path $SLURM_TMPDIR/data/CVRG-Pano \
+--fov 175.0 \
+--xi 0.0 \
+--batch-size 8
+
+
+# --cfg configs/swin/CVRG_den_unet.yaml \
